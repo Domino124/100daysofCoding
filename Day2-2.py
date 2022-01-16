1a35febfdd28517b3502c617f2df0814cc84703b
Domino124 <97834907+Domino124@users.noreply.github.com>
2022-01-16 22:10:53 +0700

    Add files via upload

---
+++ Day2-2.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+#print(type(height))
+#print(type(weight))
+num_height = float(height)
+num_weight = float(weight)
+BMI = num_weight/(num_height**2)
+print(int(BMI))
+
+
+
+
+
+
+
+
+
