c96b3ea983a232ca2162c98daab831b022a41fbe
Domino124 <97834907+Domino124@users.noreply.github.com>
2022-01-16 22:44:23 +0700

    Add files via upload

---
+++ Day2-3.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+year_left = 90 - int(age)
+day_left = year_left * 365
+week_left = year_left * 52
+month_left = year_left * 12
+print(f"You have {day_left} days, {week_left} weeks, {month_left} months, {year_left} years")
+
+
+
+
+
+
+
+
