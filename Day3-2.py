63d2c15e3532a4509ffd6467d8433d4ef4543028
Domino124 <97834907+Domino124@users.noreply.github.com>
2022-01-18 21:07:54 +0700

    Update Day3-2.py

--- Day3-2.py
+++ Day3-2.py
@@ -10,12 +10,12 @@ BMI = weight / (height**2)
 if BMI > 35:
   print(f"Your BMI is: {BMI:.0f}, you are clinically obese")
 elif BMI < 18.5:
-    print(f"Your BMI is {BMI:.0f}, you are underweight")
+  print(f"Your BMI is {BMI:.0f}, you are underweight")
 elif BMI < 25:
-    print(f"Your BMI is {BMI:.0f}, you are normal weight")
+  print(f"Your BMI is {BMI:.0f}, you are normal weight")
 elif BMI < 30:
-    print(f"Your BMI is {BMI:.0f}, you are slightly overweight")
+  print(f"Your BMI is {BMI:.0f}, you are slightly overweight")
 else: 
-    print(f"Your BMI is {BMI:.0f}, you are obese")
+  print(f"Your BMI is {BMI:.0f}, you are obese")
   
 
