fc68826601a9e579fb3d90b9172c09ab3aeb89b6
Domino124 <97834907+Domino124@users.noreply.github.com>
2022-02-05 22:59:15 +0700

    Update Day4-2.py

--- Day4-2.py
+++ Day4-2.py
@@ -8,6 +8,9 @@ names = names_string.split(", ")
 import random
 
 number = len(names)
-random_choice = random.randint(0, number - 1)
-person = names[random_choice]
+#random_choice = random.randint(0, number - 1)
+#person = names[random_choice]
+#print(person + " is going to buy the meal today")
+
+person = random.choice(names)
 print(person + " is going to buy the meal today")
