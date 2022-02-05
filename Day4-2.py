39a3978ab2854fa734f6d40d9c9752b5bb0500ca
Domino124 <97834907+Domino124@users.noreply.github.com>
2022-02-05 22:46:07 +0700

    Add files via upload

---
+++ Day4-2.py
@@ -0,0 +1,13 @@
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+#print(names)
+import random
+
+number = len(names)
+random_choice = random.randint(0, number - 1)
+person = names[random_choice]
+print(person + " is going to buy the meal today")
